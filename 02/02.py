4611336bd049c5a7aa65f991a3d1f00224f22c67
ShotaArima <g2172010@tcu.ac.jp>
2025-08-13 17:30:25 +0900

    add sklearn and create Ridge mdoel

--- 02/02.py
+++ 02/02.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+from sklearn.linear_model import Ridge
 
 # データの読み取り
 path = 'data/realestate_train.csv'
@@ -12,4 +13,20 @@ if path is not None:
 else:
     df = pd.read_csv('02/data/realestate_train.csv')
     print(df.head())
+
+# 特徴量と正解データの設定
+
+target_col = "rent_price"
+feature_cols = ['house_area', 'distance']
+
+y = df[target_col]
+X = df[feature_cols]
+
+# モデルの学習
+model = Ridge()
+model.fit(X, y)
+
+# 予測
+df['pred_rent_price'] = model.predict(X)
+print(df[['rent_price', 'pred_rent_price']])
 
